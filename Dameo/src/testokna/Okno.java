ea59c15628873014db99e2cf581ec56c74604134
Žan <zan.sifrer@gmail.com>
2018-05-03 09:59:34 +0200

    Nov paket za moznost igranja igre

---
+++ Dameo/src/testokna/Okno.java
@@ -0,0 +1,16 @@
+package testokna;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Okno extends JFrame{
+	private Platno platno;
+	
+	public Okno() {
+		super();
+		setTitle("Dameo");
+		platno = new Platno();
+		this.add(this.platno);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
