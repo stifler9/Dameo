75cdb22e8f35c6a128fd48fc85ff485b8169a1fc
Žan Šifrer <zan.sifrer@gmail.com>
2018-05-19 15:06:49 +0200

    JFrame se zažene v default package

---
+++ Dameo/src/Dameo.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+import uporabniskiVmesnik.Okno;
+
+public class Dameo {
+
+	public static void main(String[] args) {
+		JFrame glavnoOkno = new Okno();
+		glavnoOkno.pack();
+		glavnoOkno.setVisible(true);
+	}
+
+}
